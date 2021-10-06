7bb1a63e7ac076e52e892c45afa61a2cb9bd1c84
kekesibence <bence.kekesi@gmail.com>
2021-10-06 11:23:21 +0200

    Feladat elkezdése

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-index
+<!DOCTYPE html>
+<html lang=hu dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>HTML Tutorial</title>
+  </head>
+  <body>
+    <h1>Rólam</h1>
+
+  </body>
+</html>
