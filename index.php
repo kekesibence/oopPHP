3b2351a3e32caa295714e577901ce828bf8cac2c
kekesibence <bence.kekesi@gmail.com>
2021-10-06 12:04:09 +0200

    haromszoroz feladat elkeszitese

--- index.php
+++ index.php
@@ -5,21 +5,46 @@
     <title>HTML Tutorial</title>
   </head>
   <body>
-    <h1>Duplézóa/h1>
-    <<?php
-      $bence = new rolam();
-      $bence-> nevem();
-      $bence->foglalkozasom();
-      $bence->idezetem();
+    <h1>Háromszoros</h1>
+    <?php
+      $haromszoroz = new vegrehajt();
+      $tagek = new tagek();
+      $haromszoroz->ertek = 3;
+      $haromszoroz->mennyiszer = 7;
+      $haromszoroz->cmd_haromszoroz();
+      $tagek->p_eleje();
+      $haromszoroz->kiirat();
+      $tagek->p_vege();
      ?>
   </body>
 </html>
 <?php
-  class rolam
-  {
-    public function nevem()
-    {
 
+  class vegrehajt
+  {
+    public $ertek = 0;
+    public $mennyiszer = 0;
+    public function cmd_haromszoroz()
+    {
+        $this->ertek = $this->ertek * $this->mennyiszer;
     }
-}
+    public function kiirat()
+    {
+        echo $this->ertek;
+    }
+  }
+  class tagek
+  {
+
+    public function p_eleje() {
+      echo "<h2>";
+    }
+    public function p_vege() {
+      echo "</h2>";
+    }
+
+
+  }
+
+
  ?>
